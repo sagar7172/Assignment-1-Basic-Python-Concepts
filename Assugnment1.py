4144c6cebfd3e8b3b3018aed8058068b02bdf38c
Sagar Sawant <sagarsawant38@gmail.com>
2025-08-21 23:22:51 +0530

    Update Assugnment1.py

--- Assugnment1.py
+++ Assugnment1.py
@@ -1,1 +1,20 @@
+# Task 1: Perform basic mathematical operations
+first = input("Enter the first number :")
+second = input("Enter the second number :")
+Addition = int(first) + int(second)
+Subtraction = int(first) - int(second)
+Multiplication = int(first) * int(second)
+Division = int(first) / int(second)
+print("\n")
+print("Addition:", Addition)
+print("Subtraction:", Subtraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
+
+print("\n")
+# Task 2: Create a personalized greeting
+firstName = input("Enter the first name :")
+lastName = input("Enter the last name :")
+print("\n")
+print("Hello, " + firstName + " " + lastName + "! Welcome to the Python program")
 
